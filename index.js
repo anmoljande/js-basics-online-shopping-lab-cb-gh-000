ff8214589e2b323f736d0a647d7f2310106cd185
Anmol Jande <jandanmol@gmail.com>
2019-01-12 08:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ return "That item is not in your cart.";
 function placeOrder(cardNumber) {
   
   
-  if(arguments[0]==undefined)
+  if(cardNumber===undefined)
   {
     return "Sorry, we don't have a credit card on file for you.";
   }
